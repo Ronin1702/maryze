b630df5fe19e7b8e7ffca5813d06cd6ff71c1e2d
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-08-07 22:19:36 -0400

    edit/update ./models data

---
+++ seeds/user.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+
+const userData = {
+    {
+        username: 'lolol',
+        password: '12345'
+    },
+    {
+        username: 'awdsdwad'
+    }
+}
+
+
+
+
+
+
+
+
+
+
+const seedUser = () => USERMODELNAME.bulkCreate()
