a79275c799612a1a586605f449e54f6bfc534353
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-08-10 16:02:36 -0600

    fixed some error

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,14 @@
-const sequelize = require ('../config/connection');
+const sequelize = require('../config/connection');
 const coverLetter = require('./cover-letter');
 const prompt = require('./prompt');
 const user = require('./user');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
+    await user();
+    await prompt();
+    await coverLetter();
+    process.exit(0);
+};
 
-    await 
-}
+seedAll();
