92b95e1d4950ac521dbdf41843402d0c4cda06b6
Jianxiong Chen <chenjianxiong1997@gmail.com>
2023-08-09 18:59:40 -0600

    added create route

--- controllers/Logged-in-routes.js
+++ controllers/Logged-in-routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const sequelize = require('sequelize');
-const { Letters, Prompt } = require('../models');
+const { Letters, Prompt, User } = require('../models');
 const withAuth = require('../utils/auth')
 
-outer.get('/', withAuth, async (req, res) => {
+
+// go to /dashboard, if you are logged in, you will get all cover letters you alrady have
+router.get('/', withAuth, async (req, res) => {
     try {
         const cvData = await Letters.findAll({
             where: {
@@ -41,9 +43,61 @@ outer.get('/', withAuth, async (req, res) => {
         res.render('dashboard', { covers, logged_in: true });
     } catch (err) {
         console.log(err);
+        res.status(500).json({
+            message: 'Failed to render dashboard',
+            error: err
+        });
+    }
+});
+
+//  this is /dashboard/edit/:id
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+        const cvData = await Letters.findOne({
+            where: {
+                id: req.params.id,
+            },
+            attributes: ['id', 'prompt_id', 'user_id', 'created_at'],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Prompt,
+                    attributes: [
+                        'id',
+                        'full_name',
+                        'email',
+                        'company_name',
+                        'job_title',
+                        'work_exp',
+                        'education_exp',
+                        'rel_skills1',
+                        'rel_skills2',
+                        'rel_skills3'],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+            ],
+        });
+
+        if (!cvData) {
+            res.status(404).json({ message: 'No cover letter found' });
+            return;
+        }
+
+        const cover = cvData.get({ plain: true });
+        res.render('editpost', { cover, logged_in: true });
+    } catch (err) {
+        console.log(err);
         res.status(500).json(err);
     }
 });
+
+
 
 
 module.exports = router;
