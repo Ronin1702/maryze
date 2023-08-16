6b8657c220394abb431ec46ef2c6a7a23f1c585a
Ronin1702 <kchen@me.com>
2023-08-16 00:13:07 -0400

    added comments

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,20 @@
+// Define an asynchronous function to handle user logout
 const logout = async () => {
+    // Send a GET request to the server's logout endpoint to initiate the logout process
     const response = await fetch('/api/users/logout', {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
     });
 
+    // If the server responds positively (HTTP 200-299)
     if (response.ok) {
+        // Redirect the user to the login page after successful logout
         document.location.replace('/login');
     } else {
+        // If there's an issue with the logout, display the server's error message to the user
         alert(response.statusText);
     }
 };
 
+// Attach the 'logout' function to the click event of the element with id 'logout'
 document.querySelector('#logout').addEventListener('click', logout);
--- public/js/sign-up.js
+++ public/js/sign-up.js
@@ -1,25 +1,38 @@
+// Define an asynchronous function to handle the user signup process
 async function signup(event) {
+    // Prevent the default action of the form submission (e.g., page refresh)
     event.preventDefault();
 
+    // Retrieve the values input by the user for username, email, and password from the form, and trim any excess whitespace
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    console.log(username, email, password)
+    // Log the entered details to the console (for debugging purposes)
+    console.log(username, email, password);
+
+    // Ensure all fields have values before proceeding
     if (username && password && email) {
+        // Send a POST request to the server's signup endpoint with the user's details
         const response = await fetch('/api/users', {
             method: 'POST',
             body: JSON.stringify({ username, email, password }),
             headers: { 'Content-Type': 'application/json' }
         });
-        if (response.ok) {
-            console.log('You just created a new account!')
-            document.location.replace('/dashboard');
 
+        // If the server responds positively (e.g., successful registration, HTTP 200-299)
+        if (response.ok) {
+            // Log the success message to the console (for debugging)
+            console.log('You just created a new account!');
+
+            // Redirect the user to the dashboard page after successful signup
+            document.location.replace('/dashboard');
         } else {
-            alert('Sign Up Failed:Username Already Exist,Email Already Used, or Password too Short');
+            // If the signup request fails, alert the user about potential reasons
+            alert('Sign Up Failed: Username Already Exist, Email Already Used, or Password too Short');
         }
     }
 }
 
+// Attach the 'signup' function to the 'submit' event of the form with id 'signup-form'
 document.querySelector('#signup-form').addEventListener('submit', signup);
